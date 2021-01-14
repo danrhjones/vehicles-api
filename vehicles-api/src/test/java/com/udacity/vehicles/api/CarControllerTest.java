a52129460b560af890b776a11f932e7612fd0dc8
Dan Jones <dan.rh.jones@gmail.com>
2021-01-14 20:11:39 +0000

    added tests

--- vehicles-api/src/test/java/com/udacity/vehicles/api/CarControllerTest.java
+++ vehicles-api/src/test/java/com/udacity/vehicles/api/CarControllerTest.java
@@ -35,6 +35,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 
 /**
  * Implements testing of the CarController class.
@@ -79,6 +80,7 @@ public class CarControllerTest {
     @Test
     public void createCar() throws Exception {
         Car car = getCar();
+        car = carService.save(car);
         mvc.perform(
                 post(new URI("/cars"))
                         .content(json.write(car).getJson())
@@ -93,15 +95,11 @@ public class CarControllerTest {
      */
     @Test
     public void listCars() throws Exception {
-        /**
-         * TODO: Add a test to check that the `get` method works by calling
-         *   the whole list of vehicles. This should utilize the car from `getCar()`
-         *   below (the vehicle will be the first in the list).
-         */
-//        Car car = getCar();
-//        mvc.perform(
-//            get(new URI("/cars")))
-//            .andExpect(content().toString().contains("lkj"));
+        Car car = getCar();
+        car = carService.save(car);
+        mvc.perform(
+            get(new URI("/cars")))
+            .andExpect(status().isOk());
     }
 
     /**
@@ -110,10 +108,11 @@ public class CarControllerTest {
      */
     @Test
     public void findCar() throws Exception {
-        /**
-         * TODO: Add a test to check that the `get` method works by calling
-         *   a vehicle by ID. This should utilize the car from `getCar()` below.
-         */
+        Car car = getCar();
+        car = carService.save(car);
+        mvc.perform(
+            get(("/cars/{id}"), car.getId()))
+            .andExpect(status().isOk());
     }
 
     /**
@@ -122,11 +121,11 @@ public class CarControllerTest {
      */
     @Test
     public void deleteCar() throws Exception {
-        /**
-         * TODO: Add a test to check whether a vehicle is appropriately deleted
-         *   when the `delete` method is called from the Car Controller. This
-         *   should utilize the car from `getCar()` below.
-         */
+        Car car = getCar();
+        car = carService.save(car);
+        ResultActions blah = mvc.perform(
+            delete(("/cars/{id}"), car.getId()))
+            .andExpect(status().is2xxSuccessful());
     }
 
     /**
